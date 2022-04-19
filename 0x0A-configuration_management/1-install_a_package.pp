a3bd291e4fea77f3c984c8bde1f28733222264aa
root <root@Jeronimo.localdomain>
2022-04-19 13:17:05 -0500

    install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 #nstall a package with a version and using source
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem',
-  source   => 'https://rubygems.org',
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
