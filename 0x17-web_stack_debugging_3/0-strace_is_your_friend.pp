8e0132a64f2528631525f44874f69f1366feeda8
Daniel13713 <80985823+Daniel13713@users.noreply.github.com>
2022-05-03 22:26:06 -0500

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 #Debugging with curl, strace and fix with puppet
 exec { 'fix bug':
     command => 'sed -i "s/phpp/php/" /var/www/html/wp-settings.php',
-    path    => shell,
+    path    => '/bin/',
 }
