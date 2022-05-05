b34ca7163ef3d1c8e791746541404d0a9b9e948b
Daniel1375 <danydu1299@gmail.com>
2022-05-05 16:45:11 -0500

    fix puppet-lint

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,5 @@
 #Change the limit of open files in a query
 exec { "Change_limit":
-  command => 'sudo sed -i "s/-n 15/-n 4096/" /etc/default/nginx; sudo service nginx restart',
-  provider => shell,
+  command 	=> 'sudo sed -i "s/-n 15/-n 4096/" /etc/default/nginx; sudo service nginx restart',
+  provider 	=> shell,
 }
-
