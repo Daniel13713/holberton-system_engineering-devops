4606c58489d7f44d6dda165def498be115ebfc77
Daniel1375 <danydu1299@gmail.com>
2022-05-19 18:36:25 -0500

    web scraping 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
 """Gather data from an API"""
+
 import requests
 from sys import argv
 
-id = argv[1]
-URL_tasks = "https://jsonplaceholder.typicode.com/users/{}/todos".format(id)
-URL_user = "https://jsonplaceholder.typicode.com/users/{}".format(id)
+if __name__ == "__main__":
+    id = argv[1]
+    URL_tasks = "https://jsonplaceholder.typicode.com/users/{}/todos".format(
+        id)
+    URL_user = "https://jsonplaceholder.typicode.com/users/{}".format(id)
 
+    with requests.session() as session:
+        response_tasks = session.get(URL_tasks)
+        response_users = session.get(URL_user)
+        data_tasks = response_tasks.json()
+        data_users = response_users.json()
+        name_user = data_users["name"]
+        tasks_done = 0
+        tasks_all = len(data_tasks)
+        tasks_title = ""
+        for record in data_tasks:
+            if record["completed"]:
+                tasks_done += 1
+                tasks_title += "\t {}\n".format(record["title"])
 
-with requests.session() as session:
-    response_tasks = session.get(URL_tasks)
-    response_users = session.get(URL_user)
-    data_tasks = response_tasks.json()
-    data_users = response_users.json()
-    name_user = data_users["name"]
-    tasks_done = 0
-    tasks_all = len(data_tasks)
-    tasks_title = ""
-    for record in data_tasks:
-        if record["completed"]:
-            tasks_done += 1
-            tasks_title += "     {}\n".format(record["title"])
-
-    print("Employee {0} is done with tasks({1}/{2}):\n{3}".format(name_user,
-          tasks_done, tasks_all, tasks_title), end="")
+        print("Employee {0} is done with tasks({1}/{2}):\n{3}".format(
+            name_user, tasks_done, tasks_all, tasks_title), end="")
