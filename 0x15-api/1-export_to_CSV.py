7245eca35006611de2a341c50dd1e7b6df51bb91
Daniel1375 <danydu1299@gmail.com>
2022-05-19 18:43:36 -0500

    export to csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Gather data from an API"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    id = argv[1]
+    URL_tasks = "https://jsonplaceholder.typicode.com/users/{}/todos".format(
+        id)
+    URL_user = "https://jsonplaceholder.typicode.com/users/{}".format(id)
+
+    with requests.session() as session:
+        response_tasks = session.get(URL_tasks)
+        response_users = session.get(URL_user)
+        data_tasks = response_tasks.json()
+        data_users = response_users.json()
+        username = data_users["username"]
+        tasks_done = 0
+        tasks_all = len(data_tasks)
+        tasks_title = ""
+        for record in data_tasks:
+            completed = record["completed"]
+            title = record["title"]
+            print('"{}","{}","{}","{}"'.format(
+                id, username, completed, title))
