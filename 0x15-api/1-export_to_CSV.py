588e13d8c08896dbcf1d9791a7dabde20e434f41
Daniel1375 <danydu1299@gmail.com>
2022-05-19 20:44:04 -0500

    using csv library to write csv files

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -3,6 +3,7 @@
 
 import requests
 from sys import argv
+import csv
 
 if __name__ == "__main__":
     id = argv[1]
@@ -11,19 +12,25 @@ if __name__ == "__main__":
     URL_user = "https://jsonplaceholder.typicode.com/users/{}".format(id)
 
     with requests.session() as session:
-        response_tasks = session.get(URL_tasks)
-        response_users = session.get(URL_user)
-        data_tasks = response_tasks.json()
-        data_users = response_users.json()
-        username = data_users["username"]
-        tasks_done = 0
-        tasks_all = len(data_tasks)
-        data = ""
+        data_tasks = session.get(URL_tasks).json()
+        data_users = session.get(URL_user).json()
+        data = {}
+        list_data = []
         for record in data_tasks:
-            completed = record["completed"]
-            title = record["title"]
-            data += '"{}","{}","{}","{}"\n'.format(
-                id, username, completed, title)
-        textFile = "{}.csv".format(id)
-        with open(textFile, mode="w+", encoding="utf-8") as file:
-            file.write(data)
+            data["USER_ID"] = data_users["id"]
+            data["USERNAME"] = str(data_users["username"])
+            data["TASK_COMPLETED_STATUS"] = record["completed"]
+            data["TASK_TITLE"] = record["title"]
+            list_data.append(data.copy())
+
+    textFile = "{}.csv".format(id)
+    with open(textFile, mode="w+") as csvfile:
+        writer = csv.DictWriter(
+            csvfile,
+            fieldnames=data.keys(),
+            quoting=csv.QUOTE_ALL)
+        # Put header of the data in the first row
+        # writer.writeheader()
+        for data_dict in list_data:
+            # Add each dictionary
+            writer.writerow(data_dict)
