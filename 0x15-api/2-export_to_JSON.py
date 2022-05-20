26b19c835ef6160a6e0d2db0340e9959b0d49ccb
Daniel1375 <danydu1299@gmail.com>
2022-05-19 21:12:52 -0500

    using json library to export data in json file

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Gather data from an API"""
 
+import json
 import requests
 from sys import argv
 
@@ -11,28 +12,22 @@ if __name__ == "__main__":
     URL_user = "https://jsonplaceholder.typicode.com/users/{}".format(id)
 
     with requests.session() as session:
-        response_tasks = session.get(URL_tasks)
-        response_users = session.get(URL_user)
-        data_tasks = response_tasks.json()
-        data_users = response_users.json()
-        username = data_users["username"]
-        tasks_done = 0
-        tasks_all = len(data_tasks)
-        data = '{{"{}": ['.format(id)
-        for record in data_tasks:
-            completed = str(record["completed"]).lower()
+        tasks = session.get(URL_tasks).json()
+        users = session.get(URL_user).json()
+
+        list_data = []
+        for record in tasks:
+            completed = record["completed"]
             title = record["title"]
-            data += """
-                {{"task": "{0}", "completed": {1}, "username": "{2}"}}
-                    """.format(
-                title,
-                completed,
-                username
-            )
-            if data_tasks[tasks_all - 1] != record:
-                data += ", "
-        data += "]}"
+            username = users["id"]
+            data = {
+                "task": title,
+                "completed": completed,
+                "username": username}
+            list_data.append(data)
 
         textFile = "{}.json".format(id)
         with open(textFile, mode="w+", encoding="utf-8") as file:
-            file.write(data)
+            # write data
+            data_id = {id: list_data}
+            json.dump(data_id, file)
