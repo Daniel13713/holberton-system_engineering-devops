63c2958102d787e9d146060c847edacaa00ca7b1
Daniel1375 <danydu1299@gmail.com>
2022-05-19 21:26:36 -0500

    using json library to export data in json file with many dates

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,41 +1,35 @@
 #!/usr/bin/python3
 """Gather data from an API"""
 
+import json
 import requests
 from sys import argv
 
 if __name__ == "__main__":
-    data = "{"
-    for id in range(1, 11):
-        URL_tasks = """
-                https://jsonplaceholder.typicode.com/users/{}/todos
-                    """.format(id)
-        URL_user = "https://jsonplaceholder.typicode.com/users/{}".format(id)
 
-        with requests.session() as session:
-            response_tasks = session.get(URL_tasks)
-            response_users = session.get(URL_user)
-            data_tasks = response_tasks.json()
-            data_users = response_users.json()
-            username = data_users["username"]
-            tasks_done = 0
-            tasks_all = len(data_tasks)
-            data += '"{}": ['.format(id)
-            for record in data_tasks:
-                completed = str(record["completed"]).lower()
-                title = record["title"]
-                data += """
-                {{"username": "{2}", "task": "{0}", "completed": {1}}}
-                """.format(
-                    title, completed, username)
-                if data_tasks[tasks_all - 1] != record:
-                    data += ", "
-            if id != 10:
-                data += "], "
-            else:
-                data += "]"
-    data += "}"
+    URL_tasks = "https://jsonplaceholder.typicode.com/todos"
+    URL_user = "https://jsonplaceholder.typicode.com/users"
 
-    textFile = "todo_all_employees.json"
-    with open(textFile, mode="w+", encoding="utf-8") as file:
-        file.write(data)
+    with requests.session() as session:
+        tasks = session.get(URL_tasks).json()
+        users = session.get(URL_user).json()
+        data_id = {}
+        for user in users:
+            list_data = []
+            for record in tasks:
+                if record["userId"] == user["id"]:
+                    TASK_COMPLETED_STATUS = record["completed"]
+                    TASK_TITLE = record["title"]
+                    USERNAME = user["username"]
+                    data = {
+                        "task": TASK_TITLE,
+                        "completed": TASK_COMPLETED_STATUS,
+                        "username": USERNAME}
+                    list_data.append(data)
+
+            data_id.update({user["id"]: list_data})
+
+        textFile = "todo_all_employees.json"
+        with open(textFile, mode="w+", encoding="utf-8") as file:
+            # write data
+            json.dump(data_id, file)
