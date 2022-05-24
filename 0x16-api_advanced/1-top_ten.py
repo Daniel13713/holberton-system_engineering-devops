260c3e2b76d25dae371da3ac38c591e44999e986
Daniel1375 <danydu1299@gmail.com>
2022-05-24 12:14:03 -0500

    top ten of the post filter by score

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Number of subscribers for a user on reddit """
+
+import requests
+
+
+def top_ten(subreddit):
+    """
+    ------------------------------
+    GET request to subreddit user
+    ------------------------------
+    """
+    URL = """
+        https://www.reddit.com/r/{subreddit}/{listing}.json?limit={limit}&t={timeframe}
+        """.format(
+        subreddit=subreddit,
+        listing="top",
+        limit=100,
+        timeframe="month"
+    )
+    try:
+        with requests.session() as session:
+            res = session.get(
+                URL, headers={
+                    'User-agent': 'yourbot'}).json()
+
+            for post in res["data"]["children"][0:10]:
+                print(post["data"]["title"])
+
+    except Exception as err:
+        print("None")
